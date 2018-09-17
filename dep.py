f245c8e7cbb4ea8f82150f57e7220e626d09ef23
ardeleanraz <37247858+ardeleanraz@users.noreply.github.com>
2018-09-17 17:43:38 +0300

    Update dep.py

--- dep.py
+++ dep.py
@@ -1,31 +1,35 @@
-angajati ={}
-departamente = {}
+angajati = []
+departamente = []
+departamente_angajati=[]
 
 begin = None
 
-while begin != 'Q':
+while begin != 'q':
     if begin == '1':
-        ang = input('Numele angajatului este:')
-        angajati["angajat"]= ang
+        angajat = input('Numele angajatului este:')
+        angajati.append({"nume":angajat})
     if begin == '2':
         nume = input('Numele angajatului:')
-        if nume == ang:
+        if nume == angajat:
             tel = input('Scrieti numarul de telefon:')
-            angajati["telefon"] = tel
+            angajati.append({"telefon":tel})
         else:
             print('NU ESTE NICI UN ANGAJAT CU ACEST NUME')   
     if begin =='3':
         nume_1 = input('Scrieti numele angajatului:')
-        ID_1 = input('Scrieti ID-ul unic:')
-        angajati["ID"] = ID_1
+        if nume_1 == angajat:
+            ID_1 = input('Scrieti ID-ul unic:')
+            angajati.append({"ID":ID_1})
+        else:
+            print('NU ESTE NIMENI CU ACEST NUME!')
     if begin == '4':
-        dep = input('Numele departamentului este:')
-        departamente["Departament"] = dep
+        departament = input('Numele departamentului este:')
+        departamente.append({"departament":departament})
     if begin == '5':
         dep1 = input('Specificati departamentul:')
-        if dep1 == dep:
+        if dep1 == departament:
             ID = input('Scrieti ID-ul unic:')
-            departamente["ID"] =ID 
+            departamente.append({"ID":ID}) 
         else:
             print('NU ESTE NICI UN DEPARTAMENT CU ACEST NUME')
     if begin == '6':
@@ -34,9 +38,13 @@ while begin != 'Q':
         print(departamente)
     if begin =='8':
         print(departamente)
-        dep2 = input('Scrieti departamentul pe care il doriti:')
+        dep2 = input('Scrieti departamentul pe care il doriti:')  
+        print(angajati)   
         nume1 = input('Numele angajatului:')
-    
+        departamente_angajati.append({dep2:nume1})
+    if begin == '9':
+        print(departamente_angajati)
+      
 
     print('1:Adauga un nou angajat .')
     print('2:Adaugati un numar de telefon pentru angajat ,specificand numele acestuia')
@@ -46,8 +54,12 @@ while begin != 'Q':
     print('6:Lista angajati.')
     print('7:Lista departamente.')
     print('8:Adauga un angajat la un departament')
+    print('9:Angajatii sortati pe departamente')
     print('Q:Iesire.')
     begin = input('Choose a option!:')
+
+
+
 
 
 
