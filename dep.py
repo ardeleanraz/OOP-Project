3e9f3acb150add85e1f0c2d6e0010baff1e2d8be
ardeleanraz <37247858+ardeleanraz@users.noreply.github.com>
2018-09-19 19:28:48 +0300

    Update dep.py

--- dep.py
+++ dep.py
@@ -1,9 +1,9 @@
 n = 1
 m = 1
-angajati ={}
+
+
 angajati ={}
 departamente ={}
-
 begin = None
 
 while begin != 'q':
@@ -13,8 +13,15 @@ while begin != 'q':
         
         tel = input('Scrieti numarul de telefon:')
         angajati[n]['telefon'] = tel
+   
+        print(departamente)
+        dep_2 = input('Alegeti un departament din cele existente !:')
+        if dep_2 == departament:
+            angajati[n]['departament'] = dep_2
+        else:
+            print('NU AVEM ACEST DEPARTAMENT!')
         n = n +1
-       
+
     if begin == '2':
         departament = input('Numele departamentului este:')
         departamente[m] = {'departament': departament}
@@ -25,14 +32,5 @@ while begin != 'q':
 
     if begin == '4':
         print(departamente)
-
-    print('1:Adauga un nou angajat .')
-    print('2:Adauga un nou departament.')
-    print('3:Lista angajati.')
-    print('4:Lista departamente.')
-    print('Q:Iesire.')
-    begin = input('Choose a option!:')
-
-
 
 
