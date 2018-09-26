caf34147ce827f762675cbf4b52d207c971bde05
ardeleanraz <37247858+ardeleanraz@users.noreply.github.com>
2018-09-26 19:00:23 +0300

    Update dep.py

--- dep.py
+++ dep.py
@@ -33,9 +33,6 @@ while begin != 'q':
     if begin == '4':
         print(departamente)
     
-
-
-
     if begin == '5':
         print(departamente)
         id_departamente = input('Alegeti id-ul pentru care doriti sa printati lista angajatiilor:')
@@ -44,7 +41,27 @@ while begin != 'q':
                 print(angajat['nume'])
             else:
                 print ('NU AVEM ACEST DEPARTAMENT!')
-            
+    
+    if begin == '6':
+        print(departamente)
+        id_departament = input('Alegeti id-ul departamentului din cele existente:')
+        print(angajati)
+        id_angajat = input('Alegeti id-ul angajatului pe care doriti sa il puneti in functia de manager:')
+        if int(id_departament) in departamente and int(id_angajat) in angajati:
+            departamente[int(id_departament)]['manager_departament'] = id_angajat
+        else:
+            print('Nu avem acest departament sau acest angajat!')
+    
+    if begin =='7':
+        print(departamente)
+        id_departament = input('Alegeti id-ul departamentului in care se va face schimbarea:')
+        print(angajati)
+        id_ang = input('Alegeti id-ul unui angajat:')
+        departamente[int(id_departament)]['manager_departament'] = id_ang
+                    
+
+                 
+             
        
 
     print('1:Adauga un nou angajat .')
@@ -52,6 +69,8 @@ while begin != 'q':
     print('3:Lista angajati.')
     print('4:Lista departamente.')
     print('5:Listati angajatii in functie de departamente.')
+    print('6:Creati un manager pentru fiecare departament.')
+    print('7:Inlocuiti managerul unui departament cu un alt angajat.')
     print('Q:Iesire.')
     begin = input('Choose a option!:')
 
