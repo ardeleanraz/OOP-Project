cf6b15e3941b7a7a3193c8b6b64c989c42e82a5e
Razvan Ardelean <37247858+ardeleanraz@users.noreply.github.com>
2018-10-09 13:24:41 +0300

    Update Dep_class_object.py

--- Dep_class_object.py
+++ Dep_class_object.py
@@ -14,25 +14,124 @@ class Employee:
     def __str__(self):
         return self.name + ',' + self.telephone_number + ',' + str(self.departament_id)
 
-
     def __repr__(self):
-        return "{name} {telephone_number} {departament_id}".format(name = self.name, telephone_number = self.telephone_number ,departament_id = self.departament_id)
+        return "{name} {telephone_number} {departament_id}".format(name=self.name,
+                                                                   telephone_number=self.telephone_number,
+                                                                   departament_id=self.departament_id)
+
 
 class Departament:
     """ Represents any departament."""
+
     def __init__(self, name, manager_id):
         """
         :param str name:  name of the department
-        ::param int|None manager_id: the ID of an employee, which is the manager of this department
+        :param int|None manager_id: the ID of an employee, which is the manager of this department
         """
         self.name = name
         self.manager_id = manager_id
 
     def __str__(self):
-        return self.name + ',' + self.manager_id
+        return self.name + ',' + str(self.manager_id)
 
     def __repr__(self):
-        return "{name}  {manager_id}".format(name = self.name , manager_id = self.manager_id )
+        return "{name}  {manager_id}".format(name=self.name, manager_id=self.manager_id)
+
+
+
+class Product:
+    """Represent any product"""
+
+    def __init__(self, name, price, departament_id):
+        """
+        :param str name : name of product
+        :param str price : price of product
+        :param int|None departament_id: the ID of the department
+        """
+
+        self.name = name
+        self.price = price
+        self.departament_id = departament_id
+
+    def __str__(self):
+        return self.name + str(self.price) + str(self.departament_id)
+
+    def __repr__(self):
+        return "{name} {price} {id_departament}".format(name= self.name, price = self.price , id_departament = self.departament_id)
+
+
+
+class Sales:
+    """Represent any sales"""
+
+    def __init__(self,product_id,year , month, day , employee_id):
+        """
+        :param int|None product_id : name of product
+        :param int|None year: the year of sale
+        :param int|None month: the month of sale
+        :param int|None day : the day of sale
+        :param int|None employee_id: seller id who sell this product
+
+        """
+        self.product_id = product_id
+        self.year = year
+        self.month = month
+        self.day = day
+        self.employee_id = employee_id
+
+
+
+    def __str__(self):
+        return self.product_id + self.year + self.month + self.day + self.employee_id
+
+    def __repr__(self):
+        return "{product_id} {year}/{month}/{day} {employee_id}".format(product_id =self.product_id , year = self.year,
+                                                                        month =self.month , day= self.day ,employee_id =self.employee_id)
+
+
+
+def create_sales():
+    global x
+    if produse:
+        product_id = read_product_id()
+    else:
+        product_id = None
+    sales = Sales(product_id, None, None, None, None )
+
+    year = input('Anul in care s-a fabricat produsul:')
+    month = input('Luna in care s-a fabricat produsul:')
+    day = input('Ziua in care s-a fabricat produsul:')
+    sales = Sales(product_id ,int(year), int(month) , int(day), None)
+
+    if angajati:
+        employee_id = read_employee_id()
+    else:
+        employee_id = None
+
+    sales = Sales(product_id ,int(year), int(month) , int(day),employee_id)
+    vanzari[x] = sales
+    x = x + 1
+
+
+
+
+
+
+
+def create_product():
+    global o
+    name = input('Numele produsului este:')
+    price = input('Pretul produsului este:')
+    product = Product(name , price ,None)
+    if departamente:
+        departament_id = read_departament_id()
+    else:
+        departament_id = None
+
+    product = Employee(name , price , departament_id)
+    produse[o] = product
+    o = o + 1
+
 
 def create_users():
     global n
@@ -43,9 +142,10 @@ def create_users():
         departament_id = read_departament_id()
     else:
         departament_id = None
-    employee = Employee(name , telephone_number , departament_id)
+    employee = Employee(name, telephone_number, departament_id)
     angajati[n] = employee
     n = n + 1
+
 
 def create_departament():
     global m
@@ -59,7 +159,11 @@ def create_departament():
     departamente[m] = departament
     m = m + 1
 
+def print_product():
+    print(produse)
 
+def print_sales():
+    print(vanzari)
 
 def print_users():
     print(angajati)
@@ -67,6 +171,7 @@ def print_users():
 
 def print_departament():
     print(departamente)
+
 
 def print_users_by_dep_id():
     print(departamente)
@@ -80,11 +185,9 @@ def print_users_by_dep_id():
 def change_dep_manager():
     id_departament = read_departament_id()
 
-    id_angajat = read_employee_id()
+    employee_id = read_employee_id()
 
-    departamente[id_departament].manager_id = id_angajat
-
-
+    departamente[id_departament].manager_id = employee_id
 
 
 def numbers_of_dep_users():
@@ -101,11 +204,12 @@ def numbers_of_dep_users():
 def read_employee_id():
     print(angajati)
     try:
-        id_angajat = input('Alegeti id-ul unui angajat!')
-        if int(id_angajat) in angajati:
-            return int(id_angajat)
+        angajat_id = input('Alegeti id-ul unui angajat!')
+        if int(angajat_id) in angajati:
+            return int(angajat_id)
     except TypeError:
-        return  None
+        return None
+
 
 def read_departament_id():
     print(departamente)
@@ -117,14 +221,24 @@ def read_departament_id():
         return None
 
 
-
+def read_product_id():
+    print(produse)
+    try:
+        product_id = input('Alegeti id-ul unui produs:')
+        if int(product_id) in produse:
+            return int(product_id)
+    except TypeError:
+        return None
 
 
 m = 1
 n = 1
-
+o = 1
+x = 1
+produse = {}
 angajati = {}
 departamente = {}
+vanzari = {}
 begin = None
 while begin != 'q':
     if begin == '1':
@@ -148,6 +262,19 @@ while begin != 'q':
     if begin == '7':
         numbers_of_dep_users()
 
+    if begin == '8':
+        create_sales()
+
+    if begin == '9':
+        print_sales()
+
+    if begin =='10':
+        create_product()
+
+    if begin =='11':
+        print_product()
+
+
     print('1:Adauga un nou angajat .')
 
     print('2:Adauga un nou departament.')
@@ -161,6 +288,14 @@ while begin != 'q':
     print('6:Inlocuiti managerul unui departament cu un alt angajat.')
 
     print('7:Printati numarul de angajati dintr-un departament!')
+
+    print('8:Creati detalii despre o vanzare!')
+
+    print('9:Printati vanzarile !')
+
+    print('10:Creati un nou produs!')
+
+    print('11:Printati produsele!')
 
     print('Q:Iesire.')
 
